35fd04a3c97543306b67bfc51df3bc21210c97ba
Ryan Bender <ryan.bender.general@gmail.com>
2022-08-16 19:15:18 -0400

    experimenting with text editing

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,11 +1,13 @@
 import { firestore, postToJSON } from "../../lib/firebase"
 import { collection, addDoc, getDoc, getDocs } from "firebase/firestore";
-import { Button, Container, Typography } from "@mui/material";
-import { MouseEvent, useState } from "react";
+import { Box, Button, Container, Grid, Switch, TextareaAutosize, Typography } from "@mui/material";
+import { ChangeEvent, FormEvent, MouseEvent, useState } from "react";
 
 
 export default function Index() {
     const [output, setOutput] = useState("");
+    const [textValue, setTextValue] = useState("text")
+    const [textEditable, setTextEditable] = useState(false)
 
     const handleClick = async (event: MouseEvent) => {
         try {
@@ -15,8 +17,8 @@ export default function Index() {
             users.forEach(user => {
                 console.table(user)
             })
-            
-            
+
+
             const docRef = await addDoc(collection(firestore, "users"), {
                 first: "Ada",
                 last: "Lovelace",
@@ -29,12 +31,52 @@ export default function Index() {
         }
     }
 
-    return (
-        <Container
+    const handleValueChange = (event: FormEvent<HTMLDivElement>) => {
+        setTextValue(event.target.value)
+    }
 
+    const handleEditModeClick = (event: MouseEvent) => {
+        setTextEditable((prev) => !prev)
+    }
+
+    return (
+        <Grid
+            container
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            mt={5}
+            p={10}
         >
-            <Button onClick={handleClick} variant="contained">Test add Doc</Button>
-            <Typography variant='body1'>{output}</Typography>
-        </Container>
+            {/* <Button onClick={handleClick} variant="contained">Test add Doc</Button>
+            <Typography variant='body1'>{output}</Typography> */}
+            {/* <TextareaAutosize
+                aria-label="empty textarea"
+                placeholder=""
+                style={{ width: '40%', height: '200px' }}
+                onChange={handleValueChange}
+                value={textValue}
+            /> */}
+            <Switch onClick={handleEditModeClick}></Switch>
+            <Box
+                border='Highlight'
+                bgcolor='lightblue'
+                minHeight='30vh'
+                minWidth='40%'
+                p={1}
+            >
+                <div
+                    contentEditable={textEditable}
+                    onChange={handleValueChange}
+                    dangerouslySetInnerHTML={{ __html: textValue }}
+                    style={{
+                        background: textEditable ? 'white' : '',
+                        borderRadius: 3
+                    }}
+                >
+                </div>
+            </Box>
+
+        </Grid>
     )
 }
